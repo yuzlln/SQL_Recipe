3a1c14c57ec4083fa42c7a8e2408c9f1da633736
Woo Yujin <biolet2001@gmail.com>
2024-09-17 19:00:47 +0900

    Update Ch11 사용자 전체의 특징과 경향 찾기.sql

--- 5장 사용자를 파악하기 위한 데이터 추출/Ch11 사용자 전체의 특징과 경향 찾기.sql
+++ 5장 사용자를 파악하기 위한 데이터 추출/Ch11 사용자 전체의 특징과 경향 찾기.sql
@@ -1,30 +1,30 @@
 -- 회원과 비회원 구분
 SELECT session
-	, user_id
+    , user_id
     , stamp
     
     -- 로그를 stamp 순서로 나열하고 한번이라도 로그인한 사용자일 경우 이후 모든 로그 상태를 member로 설정
     , CASE WHEN COALESCE(MAX(user_id) OVER(PARTITION BY session ORDER BY stamp), '') <> '' THEN 'Member'
-		   ELSE 'None' END AS member_status
+	   ELSE 'None' END AS member_status
 FROM action_log;
 
 
 
 -- CONCAT 내부 CASE WHEN 구문
 SELECT sex
-	, age
-	, CONCAT(IF(age >= 20, sex, '')
-			  , CASE WHEN age >= 45 THEN 'Adult'
-				     WHEN age >= 4 THEN 'Child' END
-			) AS category
+    , age
+    , CONCAT(IF(age >= 20, sex, '')
+	      , CASE WHEN age >= 45 THEN 'Adult'
+   	             WHEN age >= 4 THEN 'Child' END
+	    ) AS category
 FROM mst_users_with_age;
 
 
 
--- CASE 구문 (벤 다이어그램)
+-- CASE 구문
 SELECT CASE has_purchase WHEN 1 THEN 'purchase'
-						 WHEN 0 THEN 'not purchase'
-						 ELSE 'any' END AS has_purchase
+			 WHEN 0 THEN 'not purchase'
+			 ELSE 'any' END AS has_purchase
 FROM action_venn_diagram;
 
 
@@ -32,6 +32,6 @@ FROM action_venn_diagram;
 -- NTILE : 같은 수로 데이터 그룹 생성
 SELECT user_id
     , SUM(amount) AS purchase_amount
-	, NTILE(10) OVER(ORDER BY SUM(amount) DESC) AS decile
+    , NTILE(10) OVER(ORDER BY SUM(amount) DESC) AS decile
 FROM action_log
 GROUP BY user_id;
