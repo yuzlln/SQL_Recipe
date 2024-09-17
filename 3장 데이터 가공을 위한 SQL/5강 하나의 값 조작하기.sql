fe5b94ee7e3d82bd8532138a71c6f11647c632bc
Woo Yujin <biolet2001@gmail.com>
2024-09-17 18:24:56 +0900

    Update 5강 하나의 값 조작하기.sql

--- 3장 데이터 가공을 위한 SQL/5강 하나의 값 조작하기.sql
+++ 3장 데이터 가공을 위한 SQL/5강 하나의 값 조작하기.sql
@@ -17,12 +17,12 @@ SELECT CAST('2024-07-27' AS date) AS dt
     
     
     
---  특정 필드 추출
+-- 특정 필드 추출
 -- 방법 1) EXTRACT 함수
 SELECT stamp
     , EXTRACT(YEAR FROM stamp) AS year -- YEAR/MONTH/DAY/HOUR/MINUTE/SECOND
 FROM (SELECT CAST('2024-07-27 12:34:56' AS datetime) AS stamp
-	  ) AS t; -- FROM절 서브쿼리 별칭 필수
+     ) AS t; -- FROM절 서브쿼리 별칭 필수
             
             
 -- 방법 2) SUBSTRING 함수
