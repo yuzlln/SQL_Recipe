9112d256bf668c37daa282343755b1323c863d6f
Woo Yujin <biolet2001@gmail.com>
2024-09-17 18:28:04 +0900

    Update 7장 하나의 테이블에 대한 조작.sql

--- 3장 데이터 가공을 위한 SQL/7장 하나의 테이블에 대한 조작.sql
+++ 3장 데이터 가공을 위한 SQL/7장 하나의 테이블에 대한 조작.sql
@@ -1,7 +1,7 @@
 -- 원래 값 + 집약 함수 (윈도 함수)
 SELECT product_id
-	, score
-	, AVG(score) OVER() AS avg_score -- score 전체 평균
+    , score
+    , AVG(score) OVER() AS avg_score -- score 전체 평균
     , AVG(score) OVER(PARTITION BY user_id) AS user_avg_score -- user_id별 core 평균
 FROM review;
 
@@ -9,7 +9,7 @@ FROM review;
 
 -- 순서 정의
 SELECT product_id
-  	, score
+    , score
     , ROW_NUMBER() OVER(ORDER BY score DESC) AS `row`        -- 유일한 순위
     , RANK() OVER(ORDER BY score DESC) AS `rank`             -- 같은 순위 레코드 뒤의 순위 번호 건너 뜀
     , DENSE_RANK() OVER(ORDER BY score DESC) as `dense_rank` -- 순위 번호 건너뛰지 않음
@@ -45,7 +45,7 @@ ORDER BY 구문이 있는 경우 : 첫 행에서 현재 행까지
 SELECT score
     -- 1) 순위 상위부터 누계 점수
     , SUM(score) OVER(ORDER BY score DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cum_score1
-    -- , SUM(score) OVER(ORDER BY score DESC) AS cum_score2와 동일한 결과값
+    -- SUM(score) OVER(ORDER BY score DESC) AS cum_score2와 동일한 결과값
   
     -- 2) 현재 행 + 앞/뒤 1행 평균
     , AVG(score) OVER(ORDER BY score DESC ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) AS loca_avg
@@ -55,7 +55,6 @@ FROM popular_products;
 
 -- 첫번째/마지막 레코드
 SELECT product_id
-
     -- 1) 윈도 내부의 가장 첫번째 레코드
     , FIRST_VALUE(product_id) OVER(ORDER BY score DESC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS `first_value`
  
@@ -67,14 +66,14 @@ FROM popular_products;
 
 -- 카테고리별 TOP1 제품
 SELECT DISTINCT category
-	, FIRST_VALUE(product_id) OVER(PARTITION BY category ORDER BY score DESC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS product_id
+    , FIRST_VALUE(product_id) OVER(PARTITION BY category ORDER BY score DESC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS product_id
 FROM popular_products;
 
 
 
 -- 행을 집약해서 구분자로 구분된 문자열로 변환
 SELECT purchase_id
-	, GROUP_CONCAT(product_id SEPARATOR ', ') AS product_ids
+    , GROUP_CONCAT(product_id SEPARATOR ', ') AS product_ids
 FROM purchase_detail_log
 GROUP BY purchase_id;
 
@@ -88,7 +87,6 @@ UNION ALL SELECT 3 AS idx;
 
 
 -- CROSS JOIN
-
 -- 한쪽 테이블의 모든 행과 다른 쪽 테이블의 모든 행을 조인
 -- 전체 행 개수 = 두 테이블의 행 개수를 곱한 값
 SELECT *
